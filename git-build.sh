0a5637cfb3f3684ad72ffeb92ddaa40889b05717
bic_lighter81 <moritzben.becker@outlook.de>
2025-03-23 12:31:40 +0100

    chore(wms): adds build pipeline

--- git-build.sh
+++ git-build.sh
@@ -1,8 +1,14 @@
 version="$1"
 branch="$2"
-echo "Dispatching broker build workflow run for version $version on branch $branch"
+echo "Dispatching build workflow runs for version $version on branch $branch"
 curl -v \
 -H "Accept: application/vnd.github.everest-preview+json" \
 -H "Authorization: token ${GITHUB_TOKEN}" \
 https://api.github.com/repos/biclighter81/carrier/dispatches \
 -d "{ \"event_type\": \"build-broker\", \"client_payload\": {\"version\": \"$version\", \"branch\": \"$branch\"} }"
+
+curl -v \
+-H "Accept: application/vnd.github.everest-preview+json" \
+-H "Authorization: token ${GITHUB_TOKEN}" \
+https://api.github.com/repos/biclighter81/carrier/dispatches \
+-d "{ \"event_type\": \"build-wms\", \"client_payload\": {\"version\": \"$version\", \"branch\": \"$branch\"} }"
