7c78b862fd5ece80e04d27800bf0b36c61de011a
bic_lighter81 <moritzben.becker@outlook.de>
2025-03-23 14:43:36 +0100

    feat(telemetry): adds shipment destination country counter

--- apps/broker/index.ts
+++ apps/broker/index.ts
@@ -21,11 +21,21 @@ const shipmentCounter = meter.createCounter('shipment_counter', {
 const shipmentCarrierCounter = meter.createCounter('shipment_carrier_counter', {
   description: 'Counts the number of shipments received for specific carrier',
 });
+const shipmentDestinationCountryCounter = meter.createCounter(
+  'shipment_country_counter',
+  {
+    description:
+      'Counts the number of shipments received for specific destination country',
+  }
+);
 app.post('/shipment', async (req, res) => {
   logger.info('Received shipment from WMS', { shipment: req.body });
   const shipment = req.body as Shipment;
   shipmentCounter.add(1, { status: 'received' });
   shipmentCarrierCounter.add(1, { carrierCode: shipment.carrier.carrierCode });
+  shipmentDestinationCountryCounter.add(1, {
+    destinationCountry: shipment.destination.country,
+  });
   res.status(200).json({
     message: 'Shipment received successfully',
   });
