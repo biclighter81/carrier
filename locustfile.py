3ca511b0e2ad6f81c7bba485aa294b8ced50a38c
Moritz Becker <moritz.becker2@rhenus.com>
2025-08-12 14:23:21 +0200

    feat: Update dashboard configuration and feature flags; enhance locust scripts for improved performance

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,12 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant_throughput
 
 class Shipment(HttpUser):
+    # Each user: 1 request per second
+    wait_time = constant_throughput(1)
+
     @task
-    def Shipment(self):
-        self.client.post('/shipment/dispatch')
+    def shipment(self):
+        self.client.post("/shipment/dispatch")
+    @task
+    def nginx(self):
+        self.client.get("/")
